47573d045c443e084e5f81eedaa270055c7faf1d
varshith02 <mosamvarshith02@gmail.com>
2019-12-09 20:21:43 +0530

    first commit

---
+++ keerti.rb
@@ -0,0 +1,11 @@
+I am sample test
+
+1) working directory - place where you working
+2) staging area - 
+3) local repository
+4) remote repository
+
+
+working directory 
+
+after git add it will go to staging area
